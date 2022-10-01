9787c6525cca32efdda908283a536f83124a873d
Habiba <habibayvonne198@gmail.com>
2022-10-01 12:24:34 +0300

    adding functionality

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let num1 = 8;
+/*let num1 = 8;
 let num2 = 2;
 document.getElementById("num1-el").textContent = num1;
 document.getElementById("num2-el").textContent = num2;
@@ -22,5 +22,103 @@ function divide() {
 function multiply() {
     let result = num1 * num2
     sumEl.textContent = "Sum: " + result
+} this was a simplistic implementation of a calculator*/k
+// addding a more realistic functionality of how actual calculators work
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+let screen = document.querySelector(".screen");
+
+function buttonClick(value) {
+    if (isNaN(parseInt(value))) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    rerender();
 }
+
+function handleNumber(value) {
+    if (buffer === "0") {
+        buffer = value;
+    } else {
+        buffer += value;
+    }
+}
+
+function handleMath(value) {
+    if (buffer === "0") {
+        // do nothing
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = value;
+
+    buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+    if (previousOperator === "+") {
+        runningTotal += intBuffer;
+    } else if (previousOperator === "-") {
+        runningTotal -= intBuffer;
+    } else if (previousOperator === "×") {
+        runningTotal *= intBuffer;
+    } else {
+        runningTotal /= intBuffer;
+    }
+}
+
+function handleSymbol(value) {
+    switch (value) {
+        case "C":
+            buffer = "0";
+            runningTotal = 0;
+            break;
+        case "=":
+            if (previousOperator === null) {
+                // need two numbers to do math
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = +runningTotal;
+            runningTotal = 0;
+            break;
+        case "←":
+            if (buffer.length === 1) {
+                buffer = "0";
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case "+":
+        case "-":
+        case "×":
+        case "÷":
+            handleMath(value);
+            break;
+    }
+}
+
+function rerender() {
+   buffer = screen.innerText;
+}
+
+function init() {
+    document
+        .querySelector(".calc-buttons")
+        .addEventListener("click", function (event) {
+            buttonClick(event.target.innerText);
+        });
+}
+
+init();
 
