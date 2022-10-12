bfb61af3b75d37e6db928d225c7715426b4dd70b
Habiba <habibayvonne198@gmail.com>
2022-10-12 15:21:16 +0300

    adding functionality

--- index.js
+++ index.js
@@ -1,124 +1,76 @@
-/*let num1 = 8;
-let num2 = 2;
-document.getElementById("num1-el").textContent = num1;
-document.getElementById("num2-el").textContent = num2;
-let sumEl =document.getElementById("sum-el");
 
-function add() {
-    let result = num1 + num2
-    sumEl.textContent = "Sum: " + result
-}
-
-function subtract() {
-    let result = num1 - num2
-    sumEl.textContent = "Sum: " + result
-}
-
-function divide() {
-    let result = num1 / num2
-    sumEl.textContent = "Sum: " + result
-}
-
-function multiply() {
-    let result = num1 * num2
-    sumEl.textContent = "Sum: " + result
-} this was a simplistic implementation of a calculator*/k
-// addding a more realistic functionality of how actual calculators work
 let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
 let screen = document.querySelector(".screen");
 
+/*examining what hapens when any button is clicked ion our aplication*/
+
 function buttonClick(value) {
-    if (isNaN(parseInt(value))) {
-        handleSymbol(value);
-    } else {
-        handleNumber(value);
-    }
-    rerender();
+    if(isNaN(value)){
+        //this is not a number
+        handleSymbol(value)
+    }else{
+        //this is a number
+        handleNumber(value)
+    }screen.innerText = buffer
 }
-
-function handleNumber(value) {
-    if (buffer === "0") {
-        buffer = value;
-    } else {
-        buffer += value;
-    }
-}
-
-function handleMath(value) {
-    if (buffer === "0") {
-        // do nothing
-        return;
-    }
-
-    const intBuffer = parseInt(buffer);
-    if (runningTotal === 0) {
-        runningTotal = intBuffer;
-    } else {
-        flushOperation(intBuffer);
-    }
-
-    previousOperator = value;
-
-    buffer = "0";
-}
-
-function flushOperation(intBuffer) {
-    if (previousOperator === "+") {
-        runningTotal += intBuffer;
-    } else if (previousOperator === "-") {
-        runningTotal -= intBuffer;
-    } else if (previousOperator === "×") {
-        runningTotal *= intBuffer;
-    } else {
-        runningTotal /= intBuffer;
-    }
-}
-
-function handleSymbol(value) {
-    switch (value) {
-        case "C":
-            buffer = "0";
-            runningTotal = 0;
+function handleSymbol(symbol){
+    switch(symbol){
+        case 'C':
+        buffer = '0'
+        runningTotal = 0
             break;
-        case "=":
-            if (previousOperator === null) {
-                // need two numbers to do math
-                return;
-            }
-            flushOperation(parseInt(buffer));
-            previousOperator = null;
-            buffer = +runningTotal;
-            runningTotal = 0;
+        case '+':
+        case '÷':
+        case '&times;':
+        case '-':
+        handleMath(symbol)
             break;
-        case "←":
-            if (buffer.length === 1) {
-                buffer = "0";
-            } else {
-                buffer = buffer.substring(0, buffer.length - 1);
-            }
-            break;
-        case "+":
-        case "-":
-        case "×":
-        case "÷":
-            handleMath(value);
-            break;
+}}
+
+ function handleMath() {
+    if(buffer ==='0'){
+        return
     }
+    const realBuffer = parseInt(buffer)
+    if(runningTotal === 0){
+        runningTotal = realBuffer
+    }else{
+        flushOperation(realBuffer)
+    }
+    previousOperator = symbol
+    buffer = '0'
 }
 
-function rerender() {
-   buffer = screen.innerText;
+function flushOperation(realBuffer){
+    
+    
 }
 
-function init() {
-    document
-        .querySelector(".calc-buttons")
-        .addEventListener("click", function (event) {
-            buttonClick(event.target.innerText);
-        });
+
+
+
+
+
+
+
+
+
+function handleNumber(numberString){
+    if (buffer === "0"){
+        buffer = numberString
+    }else{
+        buffer = buffer + numberString
+    }
+    
 }
-
-init();
-
+//create a function that sets everything up 
+function startUp() {
+    document.querySelector('.calc-buttons')
+        .addEventListener('click', function (event) {
+            console.log(event)
+            buttonClick(event.target.innerText)
+        })
+}
+startUp()
